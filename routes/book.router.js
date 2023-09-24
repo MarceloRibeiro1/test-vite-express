eed0ca51760c5ad1dcd05df17ac473af003a07f2
Marcelo Henrique Ribeiro <86325217+MarceloRibeiro1@users.noreply.github.com>
2023-09-24 17:05:30 -0300

    test: hello world

--- routes/book.router.js
+++ routes/book.router.js
@@ -4,9 +4,5 @@ const router = express.Router()
 const bookController = require('../controllers/book.controller')
 
 router.get("/", bookController.getAll)
-router.get("/:id", bookController.getById)
-router.post("/", bookController.create)
-router.put("/:id", bookController.updateById)
-router.delete("/:id", bookController.deleteById)
 
 module.exports = router
